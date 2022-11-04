41b56ca2da5bc5bc72a585758e0378e5512b085a
M.Kristóf <drzero123456@gmail.com>
2022-11-04 20:11:13 +0100

    before move

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -42,6 +42,8 @@ class Education extends Component {
   render() {
 
     const past_education = this.props.list
+    const state = this.props.education
+    const changeEvent = this.props.changeEvent
   
     const school_types = [
       {
@@ -75,23 +77,23 @@ class Education extends Component {
           <fieldset>
             <legend>Education</legend>
             <label htmlFor="school-name">School name: </label>
-            <input type="text" id="school-name" required data-state = "education"></input>
+            <input type="text" id="school-name" required name="name" value={state.name} data-html_state = "education" onChange={changeEvent}></input>
 
             <label htmlFor="school-type">School type:</label>
-            <select name="school-type" id="school-type" form = "cv-form" data-state = "education">
+            <select name="type" id="school-type"  value={state.type} form = "cv-form" data-html_state = "education" onChange={changeEvent}>
               {school_types.map((school) => (
                 <option value={school.value}>{school.label}</option>
               ))}
             </select>
 
             <label htmlFor="school-name">Course type: </label>
-            <input type="text" id="course-name" required data-state = "education"></input>
+            <input type="text" id="course-name" required data-html_state = "education"></input>
 
             <label htmlFor="school-start">Start date:</label>
-            <input type="date" id="school-start" required data-state = "education"></input>
+            <input type="date" id="school-start" required data-html_state = "education"></input>
 
             <label htmlFor="school-end">Email:</label>
-            <input type="date" id="school-end" required data-state = "education"></input>
+            <input type="date" id="school-end" required data-html_state = "education"></input>
             <button>+Add</button>
 
                 <div id="education-list">
@@ -132,7 +134,7 @@ class Work extends Component {
 
   render() {
 
-      const changeEvent = this.props.changeEvent
+      const changeEvent = this.props.changeEvent;
       const work_state = this.props.work;
       const workplaces = this.props.list;
 
@@ -141,19 +143,19 @@ class Work extends Component {
           <fieldset>
             <legend>Work Experience</legend>
             <label htmlFor="work-name">Company name:</label>
-            <input type="text" id="work-name" name="name" required data-state = "work" value={work_state.name} onChange = {changeEvent}></input>
+            <input type="text" id="work-name" name="name" required data-html_state = "work" value={work_state.name} onChange = {changeEvent}></input>
 
             <label htmlFor="work-position">Position:</label>
-            <input type="text" id="work-position" required data-state = "work" onChange={changeEvent}></input>
+            <input type="text" id="work-position" name="position" value={work_state.position} required data-html_state = "work" onChange={changeEvent}></input>
 
             <label htmlFor="work-start">Start date:</label>
-            <input type="date" id="work-start" required data-state = "work"></input>
+            <input type="date" id="work-start" name="start_date" value={work_state.start_date} required data-html_state = "work" onChange={changeEvent}></input>
 
             <label htmlFor="work-end">End date:</label>
-            <input type="date" id="work-end" required data-state = "work"></input>
+            <input type="date" id="work-end" name="end_date" value={work_state.end_date} required data-html_state = "work" onChange={changeEvent}></input>
 
             <label htmlFor="work-desc">Description</label>
-            <textarea id="work-desc" data-state = "work" />
+            <textarea id="work-desc" name="description" value={work_state.description} data-html_state = "work" onChange={changeEvent} />
             <button>+Add</button>
 
                 <div id="education-list">
@@ -190,7 +192,7 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.changeWork = this.changeWork.bind(this);
+    this.handleChange = this.handleChange.bind(this);
      
     this.state = {
         id : uniqid(),
@@ -222,17 +224,32 @@ class App extends Component {
       }  
     }
 
-    changeWork(event)  {
-      const {name,value} = event.target
-      const {state} = event.target.dataset;
+    // Handles the changes in the form
+     handleChange(event) {
+
+      const name_html = event.target.name.toString();
+      const value_html = event.target.value.toString()
+      const html_state = event.target.dataset.html_state.toString();
 
       this.setState({
-        work: {
-          [name]: value,
+        // Changes the id of the CV after each change
+        id: uniqid(),
+        // Grabs 3 of the main state object (Work, Personal or Experience)
+        [html_state] : {
+          // Inserts the last saved state
+          ...this.state[html_state],
+          // Updates it with the new value typed in the form
+          [name_html]: value_html,
+          
         }
-      })
-      console.log(this.state)
-    }
+})
+      console.log(`Html State: ${html_state}`)
+      console.log(name_html)
+      console.log(value_html)
+      console.log(this.state[html_state])
+      
+
+     }
 
   render() {
 
@@ -277,10 +294,10 @@ class App extends Component {
             <Personal />
           </div>
           <div id="education" className = "cv-section">
-            <Education list = {EDUCATION_MOCK}/>
+            <Education list = {EDUCATION_MOCK} education = {this.state.education} changeEvent = {this.handleChange}/>
           </div>
           <div id="work" className="cv-section">
-            <Work list = {WORK_MOCK} work = {this.state.work} changeEvent = {this.changeWork}/>
+            <Work list = {WORK_MOCK} work = {this.state.work} changeEvent = {this.handleChange}/>
           </div>
           <input type="submit"></input>
         </form>

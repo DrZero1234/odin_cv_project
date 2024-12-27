8657f3b674cf398a2738122468cc0ce83d9087b2
M.Kristóf <drzero123456@gmail.com>
2024-12-27 13:43:33 +0100

    Form fundamentals

--- cv-project/src/App.jsx
+++ cv-project/src/App.jsx
@@ -1,15 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
-import { PDFViewer } from "@react-pdf/renderer";
+// Example template: https://app.cvmaker.com/#/history/
 
-import { JobExperience } from "./components/JobExperience";
-import { Education } from "./components/Education";
-import { Cvfile } from "./components/CvFile";
+//EXAMPLE STATE
 
-// Example template: https://www.jotform.com/build/242695954884376?s=templates&salesforceTemplate=1
+/*
 
-function App() {
-  const [cvState, setCvState] = useState({
+{
     firstName: "",
     lastName: "",
     birthDate: "",
@@ -25,7 +24,7 @@ function App() {
         schoolField: "",
         studyStartDate: "",
         studyEndDate: "",
-      }*/
+      }
     ],
     experience: [
       /*{
@@ -34,196 +33,132 @@ function App() {
         description: "",
         workStartDate: "",
         workEndDate: ""
-      }*/
-    ],
-  });
-
-  const [showPreview, setShowPreview] = useState(true);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setCvState((prevState) => ({ ...prevState, [name]: value }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("Form submitted");
-  };
-
-  useEffect(() => {
-    const handleSize = () => {
-      if (window.innerWidth >= 720) {
-        setShowPreview(true);
       }
-    };
+    ],
+  }
+*/
 
-    window.addEventListener("resize", handleSize);
+let renderCount = 0;
 
-    return () => window.removeEventListener("resize", handleSize);
+const defaultValues = {
+  firstName: "",
+  lastName: "",
+  birthDate: "",
+  email: "",
+  phone: "",
+  address: "",
+  city: "",
+  zipCode: "",
+  educations: [],
+  workExperience: [],
+};
+
+function App() {
+  const [disabled, setDisabled] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    control,
+  } = useForm({
+    defaultValues,
+    disabled,
+    delayError: 1000,
   });
 
+  const sleep = (delay) => {
+    return new Promise((res) => setTimeout(res, delay));
+  };
+
+  const onSubmit = async (data) => {
+    setDisabled(true);
+    await sleep(2000);
+    setDisabled(false);
+    await console.log(data);
+  };
+
+  const onError = (errors) => console.error(errors);
+
+  const formData = watch();
+
+  console.log(formData);
+
+  renderCount++;
   return (
     <>
-      <div className="page-wrapper">
-        <header className="header">
-          <div className="container">
-            <h1 className="title page--title">CV maker</h1>
-          </div>
-        </header>
-
-        <main className="main">
-          <div className="container row">
-            <div className="user--page">
-              <div className="title--wrapper">
-                <h1 className="title">Create your own CV</h1>
-              </div>
-              <form
-                onSubmit={(e) => handleSubmit(e)}
-                className="form"
-                id="CVForm"
-              >
-                <h2 className="section--title">Personal details</h2>
-                <div className="form--row">
-                  <div className="formfield--wrapper">
-                    <label htmlFor="firstName">
-                      First name<span aria-label="required">*</span>
-                    </label>
-                    <input
-                      id="firstName"
-                      type="text"
-                      name="firstName"
-                      value={cvState.firstName}
-                      onChange={handleChange}
-                      required
-                    ></input>
-                    <span></span>
-                  </div>
-                  <div className="formfield--wrapper">
-                    <label htmlFor="lastName">
-                      Last name<span aria-label="required">*</span>
-                    </label>
-                    <input
-                      id="lastName"
-                      type="text"
-                      name="lastName"
-                      value={cvState.lastName}
-                      onChange={handleChange}
-                      required
-                    ></input>
-                  </div>
-                </div>
-
-                <div className="formfield--wrapper">
-                  <label htmlFor="birthDate">
-                    BirthDate <span aria-label="required">*</span>
-                  </label>
-                  <input
-                    type="date"
-                    value={cvState.birthDate}
-                    onChange={handleChange}
-                    name="birthDate"
-                    id="birthDate"
-                  ></input>
-                </div>
-                <div className="form--row">
-                  <div className="formfield--wrapper">
-                    <label htmlFor="email">
-                      Email<span aria-label="required">*</span>
-                    </label>
-                    <input
-                      id="email"
-                      type="email"
-                      name="email"
-                      value={cvState.email}
-                      onChange={handleChange}
-                      required
-                    ></input>
-                  </div>
-                  <div className="formfield--wrapper">
-                    <label htmlFor="phone">
-                      Phone<span aria-label="required">*</span>
-                    </label>
-                    <input
-                      id="phone"
-                      type="tel"
-                      name="phone"
-                      value={cvState.phone}
-                      onChange={handleChange}
-                      required
-                    ></input>
-                  </div>
-                </div>
-                <div className="formfield--wrapper">
-                  <label htmlFor="address">
-                    Address<span aria-label="required">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    name="address"
-                    id="address"
-                    value={cvState.address}
-                    onChange={handleChange}
-                    required
-                  ></input>
-                </div>
-                <div className="formfield--wrapper">
-                  <label htmlFor="city">
-                    City<span aria-label="required">*</span>
-                  </label>
-                  <input
-                    id="city"
-                    type="text"
-                    name="city"
-                    value={cvState.city}
-                    onChange={handleChange}
-                    required
-                  ></input>
-                </div>
-                <div className="formfield--wrapper">
-                  <label htmlFor="zipCode">
-                    ZIP code<span aria-label="required">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    name="zipCode"
-                    id="zipCode"
-                    inputMode="numeric"
-                    value={cvState.zipCode}
-                    onChange={handleChange}
-                    minLength={5}
-                    maxLength={5}
-                    required
-                  ></input>
-                </div>
-              </form>
-              <h2 className="section--title">Education</h2>
-              <Education cvState={cvState} setCvState={setCvState} />
-              <h2 className="section--title">Work Experience</h2>
-              <JobExperience
-                cvState={cvState}
-                setCvState={setCvState}
-              />
-              <button type="submit" form="CVForm">
-                Generate CV
-              </button>
-
-              <button
-                className="previewBtn"
-                onClick={() => setShowPreview(!showPreview)}
-              >
-                {showPreview ? "Hide preview" : "Show preview"}
-              </button>
-            </div>
-            <div className="preview">
-              {showPreview && (
-                <PDFViewer>
-                  <Cvfile cvState={cvState} />
-                </PDFViewer>
-              )}
-            </div>
-          </div>
-        </main>
-      </div>
+      <h1>Form {renderCount / 2}</h1>
+      <form onSubmit={handleSubmit(onSubmit, onError)} noValidate>
+        <div className="form--control">
+          <label>
+            First name
+            <input
+              {...register("firstName", {
+                required: "Please fill in this field",
+                minLength: {
+                  value: 4,
+                  message: "This name is too short",
+                },
+                maxLength: {
+                  value: 50,
+                  message: "This name is too long",
+                },
+              })}
+            />
+          </label>
+          <p className="error__msg">{errors.firstName?.message}</p>
+        </div>
+        <div className="form--control">
+          <label>
+            Last name
+            <input
+              {...register("lastName", {
+                required: "Please fill in this field",
+                minLength: {
+                  value: 4,
+                  message: "This name is too short",
+                },
+                maxLength: {
+                  value: 50,
+                  message: "This name is too long",
+                },
+              })}
+            />
+          </label>
+          <p className="error__msg">{errors.lastName?.message}</p>
+        </div>
+        <div className="form--control">
+          <label>
+            Email
+            <input
+              type="email"
+              {...register("email", {
+                required: "Please fill in this field",
+                pattern: {
+                  value:
+                    /^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/,
+                  message: "Invalid email format!",
+                },
+                minLength: {
+                  value: 8,
+                  message: "This email is too short (min. chars 8)",
+                },
+                validate: {
+                  notAdmin: (fieldValue) =>
+                    fieldValue !== "admin@example.com" ||
+                    "Enter a different email address",
+                  notBlacklisted: (fieldValue) =>
+                    // Doesnt work since there is no endsWith
+                    fieldValue.split("@")[1] !== "baddomain.com" ||
+                    "This domain is not supported",
+                },
+              })}
+            />
+          </label>
+          <p className="error__msg">{errors.email?.message}</p>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
--- cv-project/src/main.jsx
+++ cv-project/src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import "./fonts/Inter/Inter.ttf";
-import "./fonts/Inter/Inter-Italic.ttf";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />

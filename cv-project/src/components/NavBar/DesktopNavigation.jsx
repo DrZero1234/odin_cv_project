8b80daa755a080cdcdb5bb627854685da1b5da23
M.Kristóf <drzero123456@gmail.com>
2025-04-02 19:27:16 +0200

    todo fix mobile navbar

--- cv-project/src/components/NavBar/DesktopNavigation.jsx
+++ cv-project/src/components/NavBar/DesktopNavigation.jsx
@@ -3,8 +3,12 @@ import "./NavBar.css";
 
 export const DesktopNavigation = () => {
   return (
-    <nav className="Desktop--navigation font-Roboto-Bold hidden lg:flex">
-      <a href="#" id="page--logo">
+    <nav className="Desktop--navigation font-Roboto-Bold hidden items-center lg:flex">
+      <a
+        href="#"
+        id="page--logo"
+        className="text-4xl font-extrabold text-nowrap text-[#10529b]"
+      >
         CV Creator
       </a>
       <NavLinks />
--- cv-project/src/components/NavBar/NavLinks.jsx
+++ cv-project/src/components/NavBar/NavLinks.jsx
@@ -3,16 +3,13 @@ import "../../index.css";
 
 export const NavLinks = ({ isClicked, closeMenu }) => {
   return (
-    <nav className="nav--links absolute top-[15vh] right-0 z-10 flex min-h-[50vh] w-full flex-col justify-evenly border-t-[3px] border-t-[#10529b] bg-[#f7f7f7] lg:min-h-[15vh]">
+    <nav className="nav--links absolute top-[15vh] right-0 z-10 flex min-h-[50vh] w-full flex-col items-center justify-evenly border-t-[3px] border-t-[#10529b] bg-[#f7f7f7] lg:relative lg:top-0 lg:right-auto lg:z-0 lg:min-h-[15vh] lg:flex-row lg:justify-end lg:border-t-0 lg:bg-inherit">
       <ul className="flex flex-col items-center lg:flex-row lg:content-evenly">
         <li
           className="inline-block list-none px-2.5 py-5"
           onClick={() => isClicked && closeMenu()}
         >
-          <a
-            href="#"
-            className="font-bold text-black no-underline duration-300 hover:text-[#10529b]"
-          >
+          <a href="#" className="font-bold text-black no-underline">
             CV template
           </a>
         </li>
@@ -20,10 +17,7 @@ export const NavLinks = ({ isClicked, closeMenu }) => {
           className="inline-block list-none px-2.5 py-5"
           onClick={() => isClicked && closeMenu()}
         >
-          <a
-            href="#"
-            className="font-bold text-black no-underline duration-300 hover:text-[#10529b]"
-          >
+          <a href="#" className="font-bold text-black">
             CV examples
           </a>
         </li>
@@ -31,10 +25,7 @@ export const NavLinks = ({ isClicked, closeMenu }) => {
           className="inline-block list-none px-2.5 py-5"
           onClick={() => isClicked && closeMenu()}
         >
-          <a
-            href="#"
-            className="font-bold text-black no-underline duration-300 hover:text-[#10529b]"
-          >
+          <a href="#" className="font-bold text-black">
             Cover letter
           </a>
         </li>
@@ -42,10 +33,7 @@ export const NavLinks = ({ isClicked, closeMenu }) => {
           className="inline-block list-none px-2.5 py-5"
           onClick={() => isClicked && closeMenu()}
         >
-          <a
-            href="#"
-            className="font-bold text-black no-underline duration-300 hover:text-[#10529b]"
-          >
+          <a href="#" className="font-bold text-black">
             Resources
           </a>
         </li>
@@ -53,16 +41,13 @@ export const NavLinks = ({ isClicked, closeMenu }) => {
           className="inline-block list-none px-2.5 py-5"
           onClick={() => isClicked && closeMenu()}
         >
-          <a
-            href="#"
-            className="font-bold text-black no-underline duration-300 hover:text-[#10529b]"
-          >
+          <a href="#" className="font-bold text-black no-underline">
             FAQ
           </a>
         </li>
       </ul>
       <div className="navbar--separator hidden h-[20px] w-0.5 bg-[#d9deeb] lg:block"></div>
-      <div className="user--nav flex-col items-center">
+      <div className="user--nav flex flex-col items-center gap-4 px-5 py-2.5 lg:flex-row">
         <a className="user--acc" onClick={() => isClicked && closeMenu()}>
           My account
         </a>
